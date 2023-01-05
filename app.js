f7abe2e11ad07baba889c18bcb3393cad57ae883
Thana Prakonkla <120702223+Acup0fTea@users.noreply.github.com>
2023-01-05 14:57:15 +0700

    from btd

--- app.js
+++ app.js
@@ -1,27 +1,32 @@
-// for run main app 
+// for run main app
 
 // define express.js
-const { application } = require('express');
-const express = require('express');
-const debug = require('debug')('app');
-const morgan = require('morgan');
-const path = require('path');
+const { application } = require("express");
+const express = require("express");
+const debug = require("debug")("app");
+const morgan = require("morgan");
+const path = require("path");
 
 const app = express();
-const port =  process.env.PORT;
+const port = process.env.PORT;
 
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 
 // base static web
 app.use(express.static(path.join(__dirname, "/public/")));
 
+app.set("views", "./src/views"); //set views path
+app.set("view engine", "ejs");
+
 // manage request handlers
-app.get('/', (req, res) =>{
-    res.send('Hello World');
+app.get("/", (req, res) => {
+  res.render("index", {
+    username: "Bello-UWU",
+    customer: ["Thana", "Pawares", "Siwakorn"],
+  });
 });
 
 // app listen on PORT
-app.listen(port, () =>{
-    debug("Listening on port " + port);
-}
-);
+app.listen(port, () => {
+  debug("Listening on port " + port);
+});
